c2601041b5db8e70dfdf0b79f1d6446a5861c613
Christopher Siu <christopher@get2thecrossing.com>
2016-07-13 10:13:38 -0700

    Initial commit.

---
+++ eb_scraper.py
@@ -0,0 +1,15 @@
+# An experiment in using BeautifulSoup to scrape selected threads from forums.
+# author: Christopher (cesiu)
+# version: 13 July 2016
+
+from bs4 import BeautifulSoup
+import urllib2
+
+def main():
+    page = urllib2.urlopen("http://www.eurobricks.com/forum")
+    soup = BeautifulSoup(page.read(), "lxml")
+
+    print soup.title
+
+if __name__ == "__main__":
+    main()
